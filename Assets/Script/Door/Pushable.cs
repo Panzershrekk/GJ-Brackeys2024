e7f5e1942dafd4e4b65ab66465cf718106142d4b
Quentin 'Thornolf' Baudet <thornolf@hey.com>
2024-02-24 22:51:31 +0200

    Fix (Door): replacing tag by layer

--- Assets/Script/Door/Pushable.cs
+++ Assets/Script/Door/Pushable.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class Pushable : MonoBehaviour
 {
-  public float pushForce = 500f; // Adjust the force applied to the door
+  public float pushForce = 20f; // Adjust the force applied to the door
   public float interactDistance = 5f; // Maximum distance from which the player can interact with the door
+  public LayerMask layerMask;
 
   // Update is called once per frame
   void Update()
   {
+    Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+    Debug.DrawRay(transform.position, forward, Color.green);
+
     if (Input.GetKeyDown(KeyCode.E))
     {
+      Debug.Log("Pressing E");
       PushDoor();
     }
   }
@@ -17,26 +22,22 @@ public class Pushable : MonoBehaviour
   void PushDoor()
   {
     RaycastHit hit;
-    // Perform a raycast from the camera forward
-    if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, interactDistance))
+
+    if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, interactDistance, layerMask))
     {
-      // Check if the raycast hit an object tagged as "Door"
-      if (hit.collider.CompareTag("DOOR"))
+      if (hit.collider.GetComponent<Door>() != null)
       {
         Rigidbody doorRb = hit.collider.GetComponent<Rigidbody>();
         HingeJoint doorHinge = hit.collider.GetComponent<HingeJoint>();
         if (doorRb != null)
         {
-          // Optional: Disable the Hinge Joint to allow the door to move freely
           if (doorHinge != null)
           {
             Destroy(doorHinge);
           }
 
-          // Apply the force in the direction the player is looking
           Vector3 forceDirection = Camera.main.transform.forward;
 
-          // Apply the force at the point where the raycast hit the door to simulate a kick
           doorRb.AddForceAtPosition(forceDirection * pushForce, hit.point, ForceMode.Impulse);
         }
       }
