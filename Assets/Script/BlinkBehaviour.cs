addf099fb418181db586fff73eb39761e03205df
Robin 'LeBIGest' Grattepanche <robin.grattepanche@epitech.eu>
2024-02-25 01:38:04 +0100

    Begin blink behaviour on lights

--- Assets/Script/BlinkBehaviour.cs
+++ Assets/Script/BlinkBehaviour.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 
 public class BlinkBehaviour : MonoBehaviour
 {
     public Light spotlight;
-    // Start is called before the first frame update
-    void Start()
-    {
-        spotlight.DOIntensity(20, 1).SetLoops(-1).SetEase(Ease.Flash, 20, 10);
+
+    public int randomMax = 1000;
+
+    private int[] randomRange =  Enumerable.Range(0, 100).ToArray();
+
+    void FixedUpdate() {
+
+        int randomNumber = Random.Range(0, randomMax);
+
+        // if (DOTween.IsTweening(spotlight)) {
+        //     spotlight.transform.parent.gameObject.GetComponent<Renderer>().materials[0].DisableKeyword("_EMISSION");
+        // } else {
+        //      spotlight.transform.parent.gameObject.GetComponent<Renderer>().materials[0].EnableKeyword("_EMISSION");
+        // }
+        if (!DOTween.IsTweening(spotlight) && randomRange.Contains(randomNumber)) {
+            
+            spotlight.transform.parent.gameObject.GetComponent<MeshRenderer>().material.DOColor(Color.black, "_EmissionColor", 1).SetEase(Ease.InBounce, 100, 1);
+            spotlight.DOIntensity(0, 1).SetEase(Ease.Flash, 100, 3).OnComplete(() => spotlight.transform.parent.gameObject.GetComponent<MeshRenderer>().material.DOColor(Color.white, "_EmissionColor", 0));
+            ;
+        }
     }
 }
